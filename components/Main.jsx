83114879136ee7627dc2cf5752ce67ff7b9021a5
arcjun01 <arciaga.juno@student.greenriver.edu>
2025-10-16 13:51:25 -0700

    Implemented Css and other files

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,8 +1,23 @@
+import { count } from 'console'
 import React from 'react'
 
 export default function Main() {
     const [click, setClick] = React.useState(false)
 
-    
+    let count = 0;
+    function clicked(){
+        count = count + 1;
+        console.log(count);
+    }
+
+    return (
+        <main className="container">
+            <h1>Click Me!</h1>
+            <div className="counter">
+                <h2 className="count">0</h2>
+                <button className="plus" onClick={clicked}>{count}</button>
+            </div>
+        </main>
+    )
 }
 

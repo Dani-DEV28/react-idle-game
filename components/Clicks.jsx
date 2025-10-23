4f4170c9bb8c6aa55dacb43c2b0fd7c52883d462
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-10-23 14:36:23 -0700

    done

--- components/Clicks.jsx
+++ components/Clicks.jsx
@@ -1,7 +1,7 @@
 export default function Clicks({ clickCount, handleClick }) {
   return (
     <>
-      <p>Total Clicks:</p>
+      <p>Total Clicks: {clickCount}</p>
       <button onClick={handleClick}>Click Me!</button>
 
       {clickCount >= 100 && <h2>You Won! 🎉</h2>}
